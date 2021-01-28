09988aa6275fe2e4fd726ba7ea2ce7d98fe9009c
jerry <brownjer3@gmail.com>
2021-01-28 15:32:25 -0800

    commit tree 608f0e526abe9896eac936838ab46f8144784e4f

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+//   If the time is earlier than 12pm, return "Good Morning".
+// If the time is between 12pm and 5pm, return "Good Afternoon".
+// If the time is later than 5pm, return "Good Evening".
+  let hour = parseInt(time.split(":")[0])
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour < 17) {
+    return "Good Afternoon"
+  } else if (hour < 24) {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.querySelector("#greeting").innerText = message
+}
